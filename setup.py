b49e3c6659fa4e8e15c8ba01da87678e4d54b7f3
Dinesh Sharma <infosecfb2004@gmail.com>
2018-09-30 06:38:19 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+
+def get_arguments():
+    parser = optparse.OptionParser(description="Usage of MODE Changer author by Dinesh")
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MODE")
+    parser.add_option("-m", "--mode", dest="mode", help="New MODE")
+    (options, argument) = parser.parse_args()
+    if not options.interface:
+        parser.error("Please specify interface, use --help for more info")
+        parser.parse_args()
+    elif not options.mode:
+        parser.error("Please specify mode, use --help for more info")
+        parser.parse_args()
+    return options
+
+
+def change_mode(interface, mode):
+    print("[+] Changing MODE for " + interface + " to " + mode)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["iwconfig", interface, "mode", mode])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def get_current_mac(interface):
+    iwconfig_result = subprocess.check_output(["iwconfig", interface])
+    regex_final_result = re.search(r"\W\D\D\D\D:\D\D\D\D\D\D\D\s", iwconfig_result)
+    if regex_final_result:
+        return regex_final_result.group(0)
+    else:
+        print("[-] Could not read mode")
+
+
+options = get_arguments()
+
+current_mode = get_current_mac(options.interface)
+print("[+] Current MODE = " + str(current_mode))
+
+change_mode(options.interface, options.mode)
+
+current_mode = get_current_mac(options.interface)
+if current_mode == options.mode:
+    print("[+] MODE successfully changed to " + current_mode)
+else:
+    print("[+] MODE successfully changed to " + current_mode)
